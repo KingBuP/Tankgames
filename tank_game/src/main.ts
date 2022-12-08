f65ee2c63e20939170f4c164badad6cada058155
KingBuP <2251513906@qq.com>
2022-12-09 00:29:28 +0800

    tank_game

--- tank_game/src/main.ts
+++ tank_game/src/main.ts
@@ -1,6 +1,190 @@
-const el = document.querySelector<HTMLCanvasElement>("#canvas")!;
+import "./initcss.css"; //初始化
+import "./global.scss"; //全局
+import config from "./config";
 
-const app = el.getContext("2d")!;
+const app = document.querySelector<HTMLDivElement>("#app")!;
+app.style.width = config.canvas.width + "px";
+app.style.height = config.canvas.height + "px";
+/*
+//黑板案例
+import "./style.css";
+class Blackboard {
+  //实例必定触发的回调函数
+  constructor(
+    public el: HTMLCanvasElement = document.querySelector<HTMLCanvasElement>(
+      "#canvas"
+    )!,
+    private height: number = 500,
+    private width: number = 1200,
+    private bgColor: string = "#000",
+    private lineW: number = 3,
+    private lineEarw: number = 3,
+    private lineColor: string = "#fff",
+    private app = el.getContext("2d")!,
+    private btns: HTMLDivElement = document.createElement("div")
+  ) {
+    // 初始数据
+    this.el.width = width;
+    this.el.height = height;
+    this.initBackground(); //初始背景
+    this.bindEvent(); //绑定事件
+  }
+  //鼠标绘画事件
+  private bindEvent() {
+    this.el.addEventListener("mousedown", (event: MouseEvent) => {
+      if (event.button) return; //event.button = 2  右键   event.button = 0  左键  event.button = 1  中键
+      //定义 事件回调函数 方便清除事件
+      const callbackMove = this.drawLine.bind(this);
+      this.app.beginPath();
+      this.app.strokeStyle = this.lineColor;
+      this.app.lineWidth = this.lineW;
+      //鼠标移动-回调就一个函数直接绑定别如忘了.bind()
+      // this.el.addEventListener("mousemove", this.drawLine.bind(this));
+      this.el.addEventListener("mousemove", callbackMove);
+      //鼠标抬起
+      document.addEventListener("mouseup", () => {
+        //以下清除不掉 因为用了bind是返还的新的函数
+        // this.el.removeEventListener("mousemove", this.drawLine.bind(this));
+        this.el.removeEventListener("mousemove", callbackMove);
+      });
+    });
+  }
+  //画线动作
+  private drawLine(event: MouseEvent) {
+    this.app.lineTo(event.offsetX, event.offsetY);
+    this.app.stroke();
+  }
+  //初始背景
+  private initBackground() {
+    // 初始数据
+    this.app.fillStyle = this.bgColor;
+    this.app.fillRect(0, 0, this.width, this.height);
+    //元素后面放置按钮
+    this.btns.style.cssText = "margin-top:20px;"; //css
+    this.el.insertAdjacentElement("afterend", this.btns);
+  }
+  //清屏
+  public clear() {
+    const el = document.createElement("button");
+    el.innerText = "清屏";
+    el.style.cssText = "margin:5px;";
+    this.btns.insertAdjacentElement("afterbegin", el);
+    el.addEventListener("click", () => {
+      this.app.fillStyle = this.bgColor;
+      this.app.fillRect(0, 0, this.width, this.height);
+    });
+    return this; //链式
+  }
+
+  //设置背景
+  public setBgColor(color: string = "#000") {
+    this.bgColor = color;
+    // this.initBackground();
+    this.app.fillStyle = color;
+    this.app.fillRect(0, 0, this.width, this.height);
+    return this; //链式
+  }
+
+  //设置画笔颜色
+  public setLineColor() {
+    const colors = ["#e74c3c", "#d35400", "#1abc9c", "#f39c12", "#2c3e50"];
+    colors.reverse(); //反转
+    const container: HTMLDivElement = document.createElement("div");
+    container.style.cssText = "margin-top:10px;";
+    colors.forEach((color) => {
+      const but = document.createElement("button");
+      but.innerText = color;
+      but.classList.add("button_color");
+      but.style.cssText = "margin:5px;";
+
+      container.insertAdjacentElement("afterbegin", but);
+      //添加事件
+      but.addEventListener("click", () => {
+        this.lineColor = color;
+        this.lineW = this.lineEarw;
+        for (let i = 0; i < container.children.length; i++) {
+          let butEl = container.children[i] as HTMLButtonElement;
+          butEl.style.background = "#fff";
+        }
+        but.style.background = color;
+      });
+    });
+    this.btns.insertAdjacentElement("afterend", container);
+    return this;
+  }
+
+  //橡皮擦
+  public erase() {
+    const el = document.createElement("button");
+    el.innerText = "橡皮擦";
+    el.style.cssText = "margin:5px;";
+    this.btns.insertAdjacentElement("afterbegin", el);
+    el.addEventListener("click", () => {
+      this.lineColor = this.bgColor;
+      this.lineW = 20;
+    });
+    return this; //链式
+  }
+
+  //画笔
+  public draw() {
+    const el = document.createElement("button");
+    el.innerText = "画笔";
+    el.style.cssText = "margin:5px;";
+    this.btns.insertAdjacentElement("afterbegin", el);
+    el.addEventListener("click", () => {
+      this.lineColor = "#fff";
+      this.lineW = this.lineEarw;
+      document
+        .querySelectorAll(".button_color")
+        .forEach((el: any) => (el.style.background = "#fff"));
+    });
+    return this; //链式
+  }
+
+  //画布截屏
+  public short() {
+    const el = document.createElement("button");
+    el.innerText = "截图";
+    el.style.cssText = "margin:5px;";
+    this.btns.insertAdjacentElement("afterbegin", el);
+    const img: HTMLImageElement = document.createElement("img");
+    el.addEventListener("click", () => {
+      img.src = this.el.toDataURL("image/jpeg");
+      img.style.width = "300px";
+      this.download({ src: img.src, fileName: "123" });
+    });
+    this.btns.insertAdjacentElement("afterend", img);
+    return this; //链式
+  }
+
+  //图片下载
+  private download(config: any) {
+    const a = document.createElement("a"); // 创建一个a标签
+    a.href = config.src; // a标签的src属性赋值
+    if (config.target) {
+      a.target = config.target;
+    }
+    a.download = config.fileName; // a标签的download属性赋值
+    document.body.appendChild(a); // 添加a标签到body下
+    a.click(); // 触发a标签点击事件
+    document.body.removeChild(a); //  完成后删除a标签
+  }
+}
+
+const instance = new Blackboard();
+
+instance.setBgColor().setLineColor().clear().draw().erase().short();
+
+*/
+
+//----------------------------------
+//----------------------------------
+//----------------------------------
+
+// const el = document.querySelector<HTMLCanvasElement>("#canvas")!;
+
+// const app = el.getContext("2d")!;
 
 // app.fillStyle = "red";
 // app.fillRect(0, 0, 300, 300);
@@ -93,25 +277,25 @@ function scale(img: HTMLImageElement, el: HTMLCanvasElement) {
 }
 
 // 画布随机色块
-app.fillStyle = "#000";
-app.rect(0, 0, 300, 300);
-app.fill();
+// app.fillStyle = "#000";
+// app.rect(0, 0, 300, 300);
+// app.fill();
 // for (let i = 0; i < 1000; i++) {
 //   app.fillStyle = "#fff";
 //   app.fillRect(Math.random() * el.width, Math.random() * el.height, 5, 5);
 // }
 
-for (let i = 0; i < 20; i++) {
-  app.beginPath();
-  app.fillStyle = ["#1abc9c", "#gejfei", "#33feff", "#098klp", "#09iokj"].sort(
-    () => (Math.random() > 0.5 ? 1 : -1)
-  )[0];
-  app.arc(
-    Math.random() * el.width,
-    Math.random() * el.height,
-    10 + Math.random() * 50,
-    0,
-    2 * Math.PI
-  );
-  app.fill();
-}
+// for (let i = 0; i < 20; i++) {
+//   app.beginPath();
+//   app.fillStyle = ["#1abc9c", "#gejfei", "#33feff", "#098klp", "#09iokj"].sort(
+//     () => (Math.random() > 0.5 ? 1 : -1)
+//   )[0];
+//   app.arc(
+//     Math.random() * el.width,
+//     Math.random() * el.height,
+//     10 + Math.random() * 50,
+//     0,
+//     2 * Math.PI
+//   );
+//   app.fill();
+// }
